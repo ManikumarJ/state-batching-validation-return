4f3def07015c471237a4fb74b0b33124597fd059
ManikumarJ <manikumarj6@gmail.com>
2026-01-27 13:27:39 +0530

    initial commit

---
+++ src/routes/Approutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Approutes = () => {
+  return (
+     <Routes>
+        
+     </Routes>
+  )
+}
+
+export default Approutes
